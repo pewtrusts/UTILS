d77c7ae65876a5fa5a86ea07d6d83884b3b949b9
John Osterman <josterman@pewtrusts.org>
2019-02-27 09:48:00 -0500

    Adds colon to strings to be cleaned in cleanString()

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ export function publishWindowResize(S) { // need to pass in the StateModule
 
 export const StringHelpers = (function(){
     String.prototype.cleanString = function() { // lowercase and remove punctuation and replace spaces with hyphens; delete punctuation
-        return this.replace(/[ /]/g,'-').replace(/['"”’“‘,.!?;()&]/g,'').toLowerCase();
+        return this.replace(/[ /]/g,'-').replace(/['"”’“‘,.!?;()&:]/g,'').toLowerCase();
     };
 
     String.prototype.removeUnderscores = function() { 
