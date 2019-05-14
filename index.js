a4da572ba4aabcf62e4de576b4ce844e0178331b
John Osterman <josterman@pewtrusts.org>
2019-05-14 12:13:53 -0400

    Reverts to preset-env; better check for dataLayer

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-/* global dataLayer */
 export function GTMPush(eventLabel) {
+    var dataLayer = window.dataLayer || null;
     if (dataLayer) {
         dataLayer.push({ 'event': 'Interactive Click', 'eventData': eventLabel });
     }
