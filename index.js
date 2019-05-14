0ab44f572f443e018ac8ac036805e247518af420
John Osterman <josterman@pewtrusts.org>
2019-05-14 11:40:27 -0400

    Handles testing for dataLayer differently; explicitly sets to window.dataLayer or to null

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-/* global dataLayer */
+
 export function GTMPush(eventLabel) {
+    var dataLayer = window.dataLayer || null;
     if (dataLayer) {
         dataLayer.push({ 'event': 'Interactive Click', 'eventData': eventLabel });
     }
